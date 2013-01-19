f1015158317f151715ecfe3dfa4e31f5eb41358b
parrot-studio <parrot.studio.dev@gmail.com>
2013-01-19 15:02:10 +0900

    add roll times default

--- lib/pinzoro.rb
+++ lib/pinzoro.rb
@@ -5,7 +5,7 @@ module Pinzoro
 
   module_function
 
-  def roll(dice, times)
+  def roll(dice, times = 1)
     return unless (dice && times)
     return if dice <= 0 || times <= 0
     (1..times).map{ rand(dice) + 1 }
--- spec/pinzoro/pinzoro_spec.rb
+++ spec/pinzoro/pinzoro_spec.rb
@@ -11,7 +11,14 @@ describe Pinzoro do
       it { subject.size.should == 2 }
       it { subject.all?{|v| v >= 1 && v <= dice_size }.should be_true  }
     end
-    
+
+    context 'roll times default is 1' do
+      let(:dice_size){ 8 }
+      subject{ Pinzoro.roll(dice_size) }
+      it { subject.size.should == 1 }
+      it { subject.all?{|v| v >= 1 && v <= dice_size }.should be_true  }
+    end
+ 
   end
-  
+
 end
