f1b4906c8e1d5ccabfd4dcf249777cb808820381
parrot-studio <parrot.studio.dev@gmail.com>
2013-01-19 14:57:00 +0900

    add spec_helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,8 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'pinzoro'
+require 'rspec'
+
+RSpec.configure do |config|
+  config.color_enabled = true
+end
