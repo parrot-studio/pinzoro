105a84634ca94e65a1d2ba7adac72532e632c6c4
parrot-studio <parrot.studio.dev@gmail.com>
2013-01-23 12:56:05 +0900

    module move to pinzoro/

--- lib/pinzoro.rb
+++ lib/pinzoro.rb
@@ -1,17 +1,6 @@
 # coding: utf-8
 require "pinzoro/version"
-
-module Pinzoro
-
-  module_function
-
-  def roll(dice, times = 1)
-    return unless (dice && times)
-    return if dice <= 0 || times <= 0
-    (1..times).map{ rand(dice) + 1 }
-  end
-
-end
+require "pinzoro/roll"
 
 class Integer
 
---
+++ lib/pinzoro/roll.rb
@@ -0,0 +1,12 @@
+# coding: utf-8
+module Pinzoro
+
+  module_function
+
+  def roll(dice, times = 1)
+    return unless (dice && times)
+    return if dice <= 0 || times <= 0
+    (1..times).map{ rand(dice) + 1 }
+  end
+
+end
